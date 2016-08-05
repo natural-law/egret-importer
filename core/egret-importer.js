5b335c9455f7ecf29dbed352b82458888a36244f
zhangbin <zhangbin@cocos2d-x.org>
2016-08-05 10:21:26 +0800

    Import the animation assets.

--- core/egret-importer.js
+++ core/egret-importer.js
@@ -4,10 +4,13 @@ const Path = require('path');
 const Fs = require('fire-fs');
 const Url = require('fire-url');
 const Async = require('async');
+const Plist = require('plist');
 
 const AssetsRootUrl = 'db://assets';
 const ResFolderName = 'resource';
 const TempFolderName = 'temp';
+const ACTION_FOLDER_SUFFIX = '_action';
+const ACTION_NODE_NAME = 'sprite';
 
 var tempResPath = '';
 var projectPath = '';
@@ -71,14 +74,26 @@ function importProject(projPath, cb) {
                 });
             },
             function(next) {
-                // TODO import exml files
-                next();
+                _importAnimFiles(function (err) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+
+                    _removeTempResPath();
+                    next();
+                });
             },
             function(next) {
-                // TODO import animation files
+                // TODO import exml files
                 next();
             }
-        ], function () {
+        ], function (err) {
+            if (err) {
+                cb(err);
+                return;
+            }
+
             Editor.log('Import Egret project finished.');
             Editor.log('Resources are imported to folder : %s', newResourceUrl);
 
@@ -160,7 +175,7 @@ function _handleJsonFile(absPath, targetPath) {
         if (! jsonFiles[jsonType]) {
             jsonFiles[jsonType] = [];
         }
-        jsonFiles[jsonType].push(obj);
+        jsonFiles[jsonType].push(absPath);
     } else {
         // copy to the target path
         if (!Fs.existsSync(targetPath)) {
@@ -246,6 +261,226 @@ function _copyResources(srcPath, dstPath, resRoot) {
             }
         }
     });
+}
+
+// import animation files
+function _importAnimFiles(cb) {
+    var animFiles = jsonFiles.anim_cfg;
+    if (!animFiles || animFiles.length === 0) {
+        cb();
+        return;
+    }
+
+    var animUrls = {};
+    Async.waterfall([
+        function(next) {
+            // generate plist file for frames
+            for (var idx in animFiles) {
+                _genPlistForAnim(animFiles[idx]);
+            }
+
+            // import plist files
+            Editor.assetdb.import([tempResPath], AssetsRootUrl, false, function(err, results) {
+                _removeTempResPath();
+                next();
+            });
+        },
+        function(next) {
+            // generate the anim files for Creator
+            for (var idx in animFiles) {
+                var animFile = animFiles[idx];
+                animUrls[animFile] = _genAnimFile(animFile);
+            }
+
+            // import anim files
+            Editor.assetdb.import([tempResPath], AssetsRootUrl, false, function(err, results) {
+                _removeTempResPath();
+                next();
+            });
+        },
+        function(next) {
+            // generate prefab to use the animation Clip
+            for (var idx in animFiles) {
+                var animFile = animFiles[idx];
+                _genAnimPrefab(animFile, animUrls[animFile]);
+            }
+
+            // import prefab files
+            Editor.assetdb.import([tempResPath], AssetsRootUrl, false, function(err, results) {
+                _removeTempResPath();
+                next();
+            });
+        }
+    ], function(err) {
+        if (cb) {
+            cb(err);
+        }
+    });
+}
+
+function _genPlistForAnim(animFile) {
+    var name = Path.basename(animFile, Path.extname(animFile));
+    var relativeDir = Path.relative(resourcePath, Path.dirname(animFile));
+    var plistPath = Path.join(tempResPath, relativeDir, name + '.plist');
+    var pngName = name + '.png';
+
+    var animCfg = JSON.parse(Fs.readFileSync(animFile, 'utf8'));
+    var plistObj = {
+        metadata : {
+            format: 2,
+            realTextureFileName: pngName,
+            textureFileName: pngName
+        },
+        frames : {
+
+        }
+    };
+
+    var imgWidth = 0, imgHeight = 0;
+    for (var frameKey in animCfg.res) {
+        if (! animCfg.res.hasOwnProperty(frameKey)) {
+            continue;
+        }
+
+        var frameCfg = animCfg.res[frameKey];
+        plistObj.frames[frameKey] = {
+            frame: `{{${frameCfg.x},${frameCfg.y}},{${frameCfg.w},${frameCfg.h}}}`,
+            offset : '{0,0}',
+            rotated: false,
+            sourceColorRect: `{{0,0},{${frameCfg.w},${frameCfg.h}}}`,
+            sourceSize: `{${frameCfg.w},${frameCfg.h}}`
+        };
+
+        imgWidth = Math.max(imgWidth, frameCfg.x + frameCfg.w);
+        imgHeight = Math.max(imgHeight, frameCfg.y + frameCfg.h);
+    }
+
+    plistObj.metadata.size = `{${imgWidth},${imgHeight}}`;
+
+    var plistDir = Path.dirname(plistPath);
+    if (!Fs.existsSync(plistDir)) {
+        Fs.mkdirsSync(plistDir);
+    }
+
+    var plistContent = Plist.build(plistObj);
+    Fs.writeFileSync(plistPath, plistContent);
+}
+
+function _genAnimFile(animFile) {
+    var name = Path.basename(animFile, Path.extname(animFile));
+    var relativeDir = Path.relative(resourcePath, Path.dirname(animFile));
+    var plistUrl = Url.join(newResourceUrl, relativeDir, name + '.plist');
+
+    var animFolderPath = Path.join(tempResPath, relativeDir, name + ACTION_FOLDER_SUFFIX);
+    var animCfg = JSON.parse(Fs.readFileSync(animFile, 'utf8'));
+    if (!Fs.existsSync(animFolderPath)) {
+        Fs.mkdirsSync(animFolderPath);
+    }
+
+    // create animation clip files
+    var importedUrls = [];
+    for (var animName in animCfg.mc) {
+        var animInfo = animCfg.mc[animName];
+        var framesInfo = animInfo.frames;
+        if (!framesInfo || framesInfo.length === 0) {
+            continue;
+        }
+
+        var curveData = { paths : {} };
+        curveData.paths[ACTION_NODE_NAME] = {
+            comps : {
+                'cc.Sprite' : {
+                    spriteFrame : []
+                }
+            },
+            props : {
+                position: []
+            }
+        };
+
+        var oneFrameDuration = 1 / animInfo.frameRate;
+        for (var frameIdx in framesInfo) {
+            var frameCfg = framesInfo[frameIdx];
+            var frameValue = frameIdx * oneFrameDuration;
+
+            // generate position frames
+            curveData.paths[ACTION_NODE_NAME].props.position.push(
+                { frame: frameValue, value : [frameCfg.x, -frameCfg.y] }
+            );
+
+            // generate spriteFrame frames
+            var frameUrl = Url.join(plistUrl, frameCfg.res);
+            var uuid = Editor.assetdb.remote.urlToUuid(frameUrl);
+            if (Editor.assetdb.remote.existsByUuid(uuid)) {
+                var frame = new cc.SpriteFrame();
+                frame._uuid = uuid;
+                curveData.paths[ACTION_NODE_NAME].comps['cc.Sprite'].spriteFrame.push(
+                    { frame: frameValue, value : frame }
+                );
+            }
+        }
+
+        var animFilePath = Path.join(animFolderPath, animName + '.anim');
+        var animClip = new cc.AnimationClip();
+        animClip.sample = animInfo.frameRate;
+        animClip._name = animName;
+        animClip._duration = oneFrameDuration * animInfo.frames.length;
+        animClip.curveData = curveData;
+        var animClipStr = Editor.serialize(animClip);
+        Fs.writeFileSync(animFilePath, animClipStr);
+
+        importedUrls.push(Url.join(newResourceUrl, relativeDir, name + ACTION_FOLDER_SUFFIX, animName + '.anim'));
+    }
+
+    return importedUrls;
+}
+
+function _genAnimPrefab(animFile, animUrls) {
+    if (!animUrls || animUrls.length === 0) {
+        return;
+    }
+
+    // create prefab to use the animation clips
+    var name = Path.basename(animFile, Path.extname(animFile));
+    var relativeDir = Path.relative(resourcePath, Path.dirname(animFile));
+    var prefabPath = Path.join(tempResPath, relativeDir, name + '.prefab');
+    var node = new cc.Node('node');
+    var spNode = new cc.Node(ACTION_NODE_NAME);
+    node.addChild(spNode);
+    spNode.addComponent(cc.Sprite);
+
+    var animateComponent = node.addComponent(cc.Animation);
+    if (!animateComponent) {
+        Editor.warn('Add Animation component failed.');
+    } else {
+        // set properties for animation component
+        for (var i = 0, n = animUrls.length; i < n; i++) {
+            var clipUrl = animUrls[i];
+            var uuid = Editor.assetdb.remote.urlToUuid(clipUrl);
+            if (!uuid) {
+                continue;
+            }
+
+            var animClip = new cc.AnimationClip();
+            animClip._uuid = uuid;
+            animClip._name = Url.basenameNoExt(clipUrl);
+            animateComponent.addClip(animClip);
+
+            if (0 === i) {
+                animateComponent.defaultClip = animClip;
+                animateComponent.playOnLoad = true;
+            }
+        }
+    }
+
+    var prefabDir = Path.dirname(prefabPath);
+    if (!Fs.existsSync(prefabDir)) {
+        Fs.mkdirsSync(prefabDir);
+    }
+
+    var prefab = _Scene.PrefabUtils.createPrefabFrom(node);
+    var prefabData = Editor.serialize(prefab);
+    Fs.writeFileSync(prefabPath, prefabData);
 }
 
 module.exports = {
